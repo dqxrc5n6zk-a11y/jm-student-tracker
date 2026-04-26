dce0a3ac2a40b52df0af952bf3a8a8a8bd513989
Ramzi Abdulhadi <Ramzisa5@hotmail.com>
2026-04-26 12:55:10 -0500

    Vendor Capacitor Swift package for Xcode Cloud

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.0"),
+        .package(name: "capacitor-swift-pm", path: "../capacitor-swift-pm-local"),
         .package(name: "CapacitorCommunityBluetoothLe", path: "../../../node_modules/@capacitor-community/bluetooth-le"),
         .package(name: "CapacitorBrowser", path: "../../../node_modules/@capacitor/browser"),
         .package(name: "CapacitorHaptics", path: "../../../node_modules/@capacitor/haptics")
---
+++ ios/App/capacitor-swift-pm-local/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "capacitor-swift-pm",
+    products: [
+        .library(
+            name: "Capacitor",
+            targets: ["Capacitor"]
+        ),
+        .library(
+            name: "Cordova",
+            targets: ["Cordova"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "Capacitor",
+            path: "Capacitor/Capacitor.xcframework"
+        ),
+        .binaryTarget(
+            name: "Cordova",
+            path: "Cordova/Cordova.xcframework"
+        )
+    ]
+)
